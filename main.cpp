8e68f7bc8dc7b38cd2db541a8f79af4366726512
Luis Pérez <98924985+Luispm8@users.noreply.github.com>
2022-02-27 21:04:29 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number1;
+    int number2;
+    cout<<"programa para sumar dos numeros enteros"<<endl;
+    cout<<"ingresar primer numero a ser sumado ";
+    
+    cin >>  number1;
+    cout<<"ingresar segundo numero a ser sumado ";
+   
+    cin >>  number2;
+    cout<<"el resultado es "<<(number1+number2)<<endl;
+
+    return 0;
+}
